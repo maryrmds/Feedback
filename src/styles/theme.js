8ec13d4d51b8dd21d74ba79c5e2a1ac76085e728
Mariana Ribeiro <rms.mariana1@gmail.com>
2024-05-29 01:24:28 +0000

    Feat: Instalação e implantação do react-router-dom e do styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+    homeBackground: "#131518",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A24 #181E27 100%)",
+    textColor: "#969FAD",
+    submitButtonColor: "#FC7614",
+    white: "#FFF",
+    feedbackButtonColor: "#262E38",
+    feedBackButtonText: "#7C8798",
+}
