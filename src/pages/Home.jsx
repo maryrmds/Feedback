345e0e0c4efbe3c481976d1e6613b4a309aa6007
Mariana Ribeiro <rms.mariana1@gmail.com>
2024-05-31 21:26:37 +0000

    Style: Posicionamento do Card no centro da tela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,25 @@
+import { CardContainer } from "./Home.styles";
+
+import IconStarImg from "../assets/icon-star.svg"
+
 export function Home() {
   return (
-    <div>Home</div>
+    <CardContainer>
+      <IconContainer>
+        <img src="" alt="" />
+      </IconContainer>
+
+      <h1>Como foi o atendimento?</h1>
+      <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+
+      <ButtonContainer>
+        <botton>1</botton>
+        <botton>2</botton>
+        <botton>3</botton>
+        <botton>4</botton>
+        <botton>5</botton>
+      </ButtonContainer>
+      <button>Enviar</button>
+    </CardContainer>
   )
 }
